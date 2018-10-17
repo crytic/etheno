cb49a20f2648493a146568ea93ac446f5f211866
Evan Sultanik <evan.sultanik@trailofbits.com>
2018-10-16 21:13:43 -0400

    Rudimentary transaction rewriting for chain synchronization

--- etheno/synchronization.py
+++ etheno/synchronization.py
@@ -1,4 +1,46 @@
 from .client import EthenoClient, SelfPostingClient, jsonrpc, DATA, QUANTITY
+from .utils import decode_hex, format_hex_address
+
+def _decode_value(value):
+    if isinstance(value, int):
+        return value
+    try:
+        return decode_hex(value)
+    except Exception:
+        return None
+
+def _remap_params(params, mapping, method):
+    if isinstance(params, dict):
+        for key, value in params.items():
+            decoded = _decode_value(value)
+            if decoded is None:
+                params[key] = _remap_params(value, mapping, "%s['%s']" % (method, key))
+            elif decoded in mapping:
+                print("Converting %s parameter %s from address %x to %x" % (method, key, decoded, mapping[decoded]))
+                params[key] = format_hex_address(mapping[decoded])
+            elif key == 'data':
+                new_value = params['data']
+                for old, new in mapping.items():
+                    prev = new_value
+                    new_value = new_value.replace(format_hex_address(old), format_hex_address(new))
+                    if prev != new_value:
+                        print("Converting address %x in %s['data'] to %x" % (old, method, new))
+                if new_value != params['data']:
+                    params['data'] = new_value
+    elif isinstance(params, list) or isinstance(params, tuple):
+        for i, p in enumerate(params):
+            decoded = _decode_value(p)
+            if decoded is None:
+                params[i] = _remap_params(p, mapping, "%s['%d']" % (method, i))
+            elif decoded in mapping:
+                print("Converting %s parameter %d from address %x to %x" % (method, i, decoded, mapping[decoded]))
+                params[i] = format_hex_address(mapping[decoded])
+    else:
+        decoded = _decode_value(params)
+        if decoded is not None and decoded in mapping:
+            print("Converting %s from address %x to %x" % (method, decoded, mapping[decoded]))
+            return mapping[decoded]
+    return params
 
 def AddressSynchronizingClient(etheno_client):    
     old_create_account = getattr(etheno_client, 'create_account')
@@ -22,18 +64,7 @@ def AddressSynchronizingClient(etheno_client):
         def post(data):
             method = data['method']
             if 'params' in data:
-                params = data['params']
-                if len(params) == 1 and isinstance(params[0], dict):
-                    for key, value in params[0].items():
-                        if value in mapping:
-                            print("Converting %s parameter %s from address %x to %x" % (method, key, value, mapping[value]))
-                            params[0][key] = "%x" % mapping[value]
-                else:
-                    for i, p in enumerate(params):
-                        if p in mapping:
-                            print("Converting %s parameter %d from address %x to %x" % (method, i, p, mapping[p]))
-                            params[i] = mapping[p]
-            # TODO: Rewrite transaction
+                data['params'] = _remap_params(data['params'], mapping, method)
             return old_post(data)
 
         setattr(etheno_client, 'post', post)
