da97e628d9b4101cda749ce2441cdbf91fd5bc97
Evan Sultanik <evan.sultanik@trailofbits.com>
2018-10-17 09:28:39 -0400

    Also map filter IDs

--- etheno/synchronization.py
+++ etheno/synchronization.py
@@ -9,7 +9,7 @@ def _decode_value(value):
     except Exception:
         return None
 
-def _remap_params(params, mapping, method):
+def _remap_params(params, mapping, method, remap_data = False):
     if isinstance(params, dict):
         for key, value in params.items():
             decoded = _decode_value(value)
@@ -18,7 +18,7 @@ def _remap_params(params, mapping, method):
             elif decoded in mapping:
                 print("Converting %s parameter %s from address %x to %x" % (method, key, decoded, mapping[decoded]))
                 params[key] = format_hex_address(mapping[decoded])
-            elif key == 'data':
+            elif remap_data and key == 'data':
                 new_value = params['data']
                 for old, new in mapping.items():
                     prev = new_value
@@ -45,6 +45,7 @@ def _remap_params(params, mapping, method):
 def AddressSynchronizingClient(etheno_client):    
     old_create_account = getattr(etheno_client, 'create_account')
     mapping = {}
+    filter_mapping = {}
 
     def create_account(balance = 0, address = None):
         try:
@@ -63,9 +64,28 @@ def AddressSynchronizingClient(etheno_client):
 
         def post(data):
             method = data['method']
+            uninstalling_filter = None
             if 'params' in data:
-                data['params'] = _remap_params(data['params'], mapping, method)
-            return old_post(data)
+                data['params'] = _remap_params(data['params'], mapping, method, remap_data = True)
+                if ('filter' in method.lower() and 'get' in method.lower()) or method == 'eth_uninstallFilter':
+                    # we are accessing a filter by its ID, so remap the ID
+                    old_id = data['params'][0]
+                    if old_id not in filter_mapping:
+                        print("Warning: %s called on unknown filter ID %s; ignoring..." % (method, old_id))
+                    else:
+                        print("Mapping filter ID %s to %s for %s" % (old_id, filter_mapping[old_id], method))
+                        data['params'] = [filter_mapping[old_id]]
+                    if method == 'eth_uninstallFilter':
+                        uninstalling_filter = old_id
+            ret = old_post(data)
+            if uninstalling_filter is not None:
+                if ret['result']:
+                    # the uninstall succeeded, so we no longer need to keep the mapping:
+                    del filter_mapping[uninstalling_filter]
+            elif 'filter' in method.lower() and 'new' in method.lower() and 'result' in ret:
+                # a new filter was just created, so record the mapping
+                filter_mapping[etheno_client.etheno.rpc_client_result['result']] = ret['result']
+            return ret
 
         setattr(etheno_client, 'post', post)
     else:
