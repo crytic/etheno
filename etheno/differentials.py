8ab020f524a2c9d2c1b2f949da94a211c1223319
Evan Sultanik <evan.sultanik@trailofbits.com>
2018-10-17 16:02:15 -0400

    Started implementing the differential tester plugin (#3)

---
+++ etheno/differentials.py
@@ -0,0 +1,19 @@
+from .etheno import EthenoPlugin
+
+class DifferentialTester(EthenoPlugin):
+    def after_post(self, data, client_results):
+        method = data['method']
+        if method == 'eth_getTransactionReceipt':
+            master_result = client_results[0]
+            if master_result and 'result' in master_result and master_result['result'] and 'contractAddress' in master_result['result'] and master_result['result']['contractAddress']:
+                # the master client created a new contract
+                # so make sure that all of the other clients did, too
+                for client, data in zip(self.etheno.clients, client_results[1:]):
+                    created = False
+                    try:
+                        created = data['result']['contractAddress']
+                    except Exception:
+                        pass
+                    if not created:
+                        print("Error: the master client created a contract for transaction %s, but %s did not!" % (master_result['transactionHash'], client))
+
