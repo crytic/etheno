afb110f438eaff7fcd3a5a6499b7526a4ef7120a
Evan Sultanik <evan.sultanik@trailofbits.com>
2018-10-23 19:58:12 -0400

    Added requirements for our `keyfile.py` fork

--- setup.py
+++ setup.py
@@ -13,7 +13,11 @@ setup(
         'manticore', # TODO: specify a specific manticore version once https://github.com/trailofbits/manticore/pull/1054 makes it into a release
         'pysha3>=1.0.2',
         'flask>=1.0.2',
-        'web3'
+        'web3',
+        # The following two requirements are for our fork of `keyfile.py`,
+        # but they should already be satisfied by the `web3` requirement
+        'cytoolz>=0.9.0,<1.0.0',
+        'pycryptodome>=3.4.7,<4.0.0',
     ],
     entry_points={
         'console_scripts': [
