9b873302326521eda65de32916817c562d260baf
Evan Sultanik <evan.sultanik@trailofbits.com>
2018-10-24 21:13:30 -0400

    Colorized terminal output

--- etheno/logger.py
+++ etheno/logger.py
@@ -1,3 +1,4 @@
+import enum
 import logging
 
 CRITICAL = logging.CRITICAL
@@ -7,17 +8,63 @@ INFO     = logging.INFO
 DEBUG    = logging.DEBUG
 NOTSET   = logging.NOTSET
 
-class NonInfoFormatter(object):
-    def __init__(self, parent_formatter):
-        self._parent_formatter = parent_formatter
-        self._vanilla_formatter = logging.Formatter()
+class CGAColors(enum.Enum):
+    BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
+
+ANSI_RESET = "\033[0m"
+ANSI_COLOR = "\033[1;%dm"
+ANSI_BOLD  = "\033[1m"
+
+LEVEL_COLORS = {
+    CRITICAL: CGAColors.MAGENTA,
+    ERROR: CGAColors.RED,
+    WARNING: CGAColors.YELLOW,
+    INFO: CGAColors.GREEN,
+    DEBUG: CGAColors.CYAN,
+    NOTSET: CGAColors.BLUE
+}
+
+def formatter_message(message, use_color = True):
+    if use_color:
+        message = message.replace("$RESET", RESET_SEQ).replace("$BOLD", BOLD_SEQ)
+    else:
+        message = message.replace("$RESET", "").replace("$BOLD", "")
+    return message
+
+class ComposableFormatter(object):
+    def __init__(self, *args, **kwargs):
+        if len(args) == 1 and not isinstance(args[0], str):
+            self._parent_formatter = args[0]
+        else:
+            self._parent_formatter = self.new_formatter(*args, **kwargs)
+    def new_formatter(self, *args, **kwargs):
+        return logging.Formatter(*args, **kwargs)
+    def __getattr__(self, name):
+        return getattr(self._parent_formatter, name)
+
+class ColorFormatter(ComposableFormatter):
+    def reformat(self, fmt):
+        for color in CGAColors:
+            fmt = fmt.replace("$%s" % color.name, ANSI_COLOR % (30 + color.value))
+        fmt = fmt.replace("$RESET", ANSI_RESET)
+        fmt = fmt.replace("$BOLD", ANSI_BOLD)
+        return fmt
+    def new_formatter(self, fmt, *args, **kwargs):
+        if 'datefmt' in kwargs:
+            kwargs['datefmt'] = self.reformat(kwargs['datefmt'])
+        return super().new_formatter(self.reformat(fmt), *args, **kwargs)
+    def format(self, *args, **kwargs):
+        levelcolor = LEVEL_COLORS.get(args[0].levelno, LEVEL_COLORS[NOTSET])
+        ret = self._parent_formatter.format(*args, **kwargs)
+        return ret.replace('$LEVELCOLOR', ANSI_COLOR % (30 + levelcolor.value))
+
+class NonInfoFormatter(ComposableFormatter):
+    _vanilla_formatter = logging.Formatter()
     def format(self, *args, **kwargs):
         if args and args[0].levelno == INFO:
             return self._vanilla_formatter.format(*args, **kwargs)
         else:
             return self._parent_formatter.format(*args, **kwargs)
-    def __getattr__(self, name):
-        return getattr(self._parent_formatter, name)
 
 class EthenoLogger(object):
     def __init__(self, name, log_level=None, parent=None):
@@ -34,7 +81,7 @@ class EthenoLogger(object):
         self._handler = logging.StreamHandler()
         if log_level is not None:
             self.log_level = log_level
-        formatter = logging.Formatter('%(levelname)-8s [%(asctime)14s][%(name)s] %(message)s', datefmt='%m-%d|%H:%M:%S')
+        formatter = ColorFormatter('$RESET$LEVELCOLOR$BOLD%(levelname)-8s $BLUE[$RESET$WHITE%(asctime)14s$BLUE$BOLD][$RESET$WHITE%(name)s$BLUE$BOLD]$RESET %(message)s', datefmt='%m$BLUE-$WHITE%d$BLUE|$WHITE%H$BLUE:$WHITE%M$BLUE:$WHITE%S')
         if self.parent is None:
             formatter = NonInfoFormatter(formatter)
         self._handler.setFormatter(formatter)
