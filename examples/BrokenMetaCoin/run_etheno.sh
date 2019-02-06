5dd8177384a982f372e4156ec6fc84b738748476
Evan Sultanik <evan.sultanik@trailofbits.com>
2019-02-06 11:08:09 -0500

    Workaround for a Truffle build bug (#49)

--- examples/BrokenMetaCoin/run_etheno.sh
+++ examples/BrokenMetaCoin/run_etheno.sh
@@ -1,3 +1,10 @@
+# First, remove the Truffle build directory.
+# This shouldn't be necessary, but Truffle will often fail with
+# confusing error messages if it is upgraded between builds.
+# So, we just rebuild everything from scratch each time to ensure
+# that it always works.
+rm -rf build
+
 echo "Running the custom Manticore script ExploitMetaCoinManticoreScript.py"
 # Set the max depth for Manticore to 2 because this script only needs to
 # find a sequence of two transactions to exploit the bug
