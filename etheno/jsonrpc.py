8fdec551ca9d51c77faf559a0bb798cc7c62e96b
Evan Sultanik <evan.sultanik@trailofbits.com>
2019-03-27 10:18:33 -0400

    Started implementing a new event summary plugin (#59)

--- etheno/jsonrpc.py
+++ etheno/jsonrpc.py
@@ -1,32 +1,101 @@
 import json
-from typing import TextIO, Union
+from typing import Dict, TextIO, Union
 
 from .etheno import EthenoPlugin
 
-class JSONRPCExportPlugin(EthenoPlugin):
+class JSONExporter:
     def __init__(self, out_stream: Union[str, TextIO]):
         self._was_path = isinstance(out_stream, str)
         if self._was_path:
-            self._output = open(out_stream, 'w', encoding='utf8')
+            self.output = open(out_stream, 'w', encoding='utf8')
         else:
-            self._output = out_stream
-        self._output.write('[')
+            self.output = out_stream
+        self.output.write('[')
         self._count = 0
-
-    def before_post(self, post_data):
-        if self._count > 0:
-            self._output.write(',')
-        self._count += 1
-        self._output.write('\n')
-        json.dump(post_data, self._output)
-        self._output.flush()
+        self._finalized = False
 
     def finalize(self):
+        if self._finalized:
+            return
         if self._count:
-            self._output.write('\n')
-        self._output.write(']')
-        self._output.flush()
+            self.output.write('\n')
+        self.output.write(']')
+        self.output.flush()
         if self._was_path:
-            self._output.close()
-        if hasattr(self._output, 'name'):
-            self.logger.info(f'Raw JSON RPC messages dumped to {self._output.name}')
+            self.output.close()
+        self._finalized = True
+
+    def write_entry(self, entry):
+        if self._finalized:
+            return
+        if self._count > 0:
+            self.output.write(',')
+        self._count += 1
+        self.output.write('\n')
+        json.dump(entry, self.output)
+        self.output.flush()
+        
+
+class JSONRPCExportPlugin(EthenoPlugin):
+    def __init__(self, out_stream: Union[str, TextIO]):
+        self._exporter = JSONExporter(out_stream)
+    
+    def before_post(self, post_data):
+        self._exporter.write_entry(post_data)
+
+    def finalize(self):
+        self._exporter.finalize()
+        if hasattr(self._exporter.output, 'name'):
+            self.logger.info(f'Raw JSON RPC messages dumped to {self._exporter.output.name}')
+
+
+class EventSummaryPlugin(EthenoPlugin):
+    def __init__(self):
+        self._transactions: Dict[int, Dict[str, object]] = {} # Maps transaction hashes to their eth_sendTransaction arguments
+
+    def handle_contract_created(self, creator_address: str, contract_address: str, gas_used: str, gas_price: str, data: str):
+        self.logger.info(f'Contract created at {contract_address} with {(len(data)-2)//2} bytes of data by account {creator_address} for {gas_used} gas with a gas price of {gas_price}')
+
+    def after_post(self, post_data, result):
+        if len(result):
+            result = result[0]
+        if 'method' not in post_data:
+            return
+        elif post_data['method'] == 'eth_sendTransaction' and 'result' in result:
+            try:
+                transaction_hash = int(result['result'], 16)
+            except ValueError:
+                return
+            self._transactions[transaction_hash] = post_data
+        elif post_data['method'] == 'eth_getTransactionReceipt':
+            transaction_hash = int(post_data['params'][0], 16)
+            if transaction_hash not in self._transactions:
+                self.logger.error(f'Received transaction receipt {result} for unknown transaction hash {post_data["params"][0]}')
+                return
+            if 'to' not in result['result'] or result['result']['to'] is None:
+                # this transaction is creating a contract:
+                contract_address = result['result']['contractAddress']
+                original_transaction = self._transactions[transaction_hash]['params'][0]
+                self.handle_contract_created(original_transaction['from'], contract_address, result['result']['gasUsed'], original_transaction['gasPrice'], original_transaction['data'])
+
+
+class EventSummaryExportPlugin(EventSummaryPlugin):
+    def __init__(self, out_stream: Union[str, TextIO]):
+        super().__init__()
+        self._exporter = JSONExporter(out_stream)
+
+    def handle_contract_created(self, creator_address: str, contract_address: str, gas_used: str, gas_price: str, data: str):
+        self._exporter.write_entry({
+            'event' : 'ContractCreated',
+            'from' : creator_address,
+            'contract_address' : contract_address,
+            'gas_used' : gas_used,
+            'gas_price' : gas_price,
+            'data' : data
+        })
+        super().handle_contract_created(creator_address, contract_address, gas_used, gas_price, data)
+
+    def finalize(self):
+        self._exporter.finalize()
+        if hasattr(self._exporter.output, 'name'):
+            self.logger.info(f'Event summary JSON saved to {self._exporter.output.name}')
