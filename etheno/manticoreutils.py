f1bdb47a8edcf5f2a49c16d2d251ab103be6629c
Evan Sultanik <evan.sultanik@trailofbits.com>
2019-06-27 21:22:01 -0400

    Fixes the Manticore version check (#63)

--- etheno/manticoreutils.py
+++ etheno/manticoreutils.py
@@ -11,32 +11,39 @@ from manticore.core.smtlib.operators import AND
 from manticore.ethereum import ManticoreEVM, Detector
 import manticore.ethereum.detectors
 
+
 def manticore_version():
     return pkg_resources.get_distribution('manticore').version
 
+
 def manticore_is_new_enough():
-    '''Checks if Manticore is newer than version 0.2.2. Returns True or False if known, or None if uncertain.'''
+    """Checks if Manticore is newer than version 0.2.2. Returns True or False if known, or None if uncertain."""
     try:
         version = manticore_version()
-        version = map(int, version.split('.'))
+        version = list(map(int, version.split('.')))
         for v, required in itertools.zip_longest(version, (0, 2, 2), fillvalue=0):
             if v < required:
                 return False
-    except Exception as e:
+            elif v > required:
+                return True
+    except Exception:
         return None
     return True
+
 
 def get_detectors():
     for name, obj in inspect.getmembers(manticore.ethereum.detectors):
         if inspect.isclass(obj) and issubclass(obj, manticore.ethereum.detectors.Detector) and obj != manticore.ethereum.detectors.Detector:
             yield obj
 
+
 def register_all_detectors(manticore):
     for detector in get_detectors():
         manticore.register_detector(detector())
 
+
 class StopAtDepth(Detector):
-    ''' This just aborts explorations that are too deep '''
+    """This just aborts explorations that are too deep"""
 
     def __init__(self, max_depth):
         super().__init__()
@@ -56,14 +63,18 @@ class StopAtDepth(Detector):
             if reps[item] > self.max_depth:
                 state.abandon()
 
-class ManticoreTest(object):
+
+class ManticoreTest:
     def __init__(self, state, expression):
         self.state = state
         self.expression = expression
+
     def __bool__(self):
         return self.can_be_true()
+
     def can_be_true(self):
         return self.state.can_be_true(self.expression)
+
     def _solve_one(self, *variables, initial_state):
         with initial_state as state:
             state.constrain(self.expression)
@@ -71,11 +82,13 @@ class ManticoreTest(object):
                 value = state.solve_one(v)
                 yield value
                 state.constrain(v == value)        
+
     def solve_one(self, *variables):
-        '''Finds a solution to the state and returns all of the variables in that solution'''
+        """Finds a solution to the state and returns all of the variables in that solution"""
         return self._solve_one(*variables, initial_state=self.state)
+
     def solve_all(self, *variables):
-        '''Enumerates all solutions to the state for the given variables'''
+        """Enumerates all solutions to the state for the given variables"""
         with self.state as state:
             while state.can_be_true(self.expression):
                 solution = tuple(self._solve_one(*variables, initial_state=state))
@@ -83,10 +96,13 @@ class ManticoreTest(object):
                     break
                 yield solution
                 state.constrain(AND(*(v != s for v, s in zip(variables, solution))))
+
     def __enter__(self):
         return self
+
     def __exit__(self, exc_type, exc_val, exc_tb):
         pass
+
 
 if __name__ == '__main__':
     print('Available Manticore Detectors:')
