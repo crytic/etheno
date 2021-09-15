4c9a8b087c2389e02987190b272af3a2119a791f
Evan Sultanik <evan.sultanik@trailofbits.com>
2021-09-15 14:13:58 -0400

    Type hints and PEP compliance

--- etheno/client.py
+++ etheno/client.py
@@ -2,14 +2,17 @@ import http
 import inspect
 import json
 import time
+from typing import Any, Dict, List, Optional, Set, Union
 from urllib.request import Request, urlopen
 
 from . import logger
 from .utils import decode_hex, format_hex_address, webserver_is_up
 
+
 def jsonrpc(**types):
     def decorator(function):
         signature = inspect.getfullargspec(function).args
+
         def wrapper(self, *args, **kwargs):
             rpc_kwargs = dict(kwargs)
             return_type = None
@@ -31,36 +34,48 @@ def jsonrpc(**types):
         return wrapper
     return decorator
 
-class RpcHttpProxy(object):
+
+class RpcHttpProxy:
     def __init__(self, urlstring):
         self.urlstring = urlstring
         self.rpc_id = 0
-    def post(self, data):
+
+    def post(self, data) -> Dict[str, Union[int, str, Dict[str, Any]]]:
         data = dict(data)
         self.rpc_id += 1
-        rpc_id = self.rpc_id
         return_id = None
         if 'jsonrpc' not in data:
             data['jsonrpc'] = '2.0'
         if 'id' in data:
             return_id = data['id']
             data['id'] = self.rpc_id
-        request = Request(self.urlstring, data = bytearray(json.dumps(data), 'utf8'), headers={'Content-type': 'application/json'})
+        request = Request(
+            self.urlstring,
+            data=bytearray(json.dumps(data), 'utf8'),
+            headers={'Content-type': 'application/json'}
+        )
         ret = json.loads(urlopen(request).read())
         if return_id is not None and 'id' in ret:
             ret['id'] = return_id
         return ret
+
     def __str__(self):
-        return "%s<%s>" % (self.__class__.__name__, self.urlstring)
-    __repr__ = __str__
+        return f"{self.__class__.__name__}<{self.urlstring}>"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.urlstring})"
+
 
 class JSONRPCError(RuntimeError):
     def __init__(self, client, data, result):
-        super().__init__("JSON RPC Error in Client %s when processing transaction:\n%s\n%s" % (client, data, result['error']))
+        super().__init__(
+            "JSON RPC Error in Client %s when processing transaction:\n%s\n%s" % (client, data, result['error'])
+        )
         self.client = client
         self.json = data
         self.result = result
-    
+
+
 def transaction_receipt_succeeded(data):
     if not (data and 'result' in data and data['result']):
         return None
@@ -76,8 +91,9 @@ def transaction_receipt_succeeded(data):
     elif not isinstance(status, int):
         status = int(status, 16)
     return status > 0
-    
-class EthenoClient(object):
+
+
+class EthenoClient:
     _etheno = None
     logger = None
     _short_name = None
@@ -100,20 +116,19 @@ class EthenoClient(object):
         self.etheno_set()
 
     @property
-    def log_directory(self):
-        '''Returns a log directory that this client can use to save additional files, or None if one is not available'''
+    def log_directory(self) -> Optional[str]:
+        """Returns a log directory that this client can use to save additional files, or None if one is not available"""
         if self.logger is None:
             return None
         else:
             return self.logger.directory
 
     def etheno_set(self):
-        '''A callback for once the etheno instance and logger for this client is set'''
+        """A callback for once the etheno instance and logger for this client is set"""
         pass
 
     def create_account(self, balance = 0, address = None):
-        '''
-        A request for the client to create a new account.
+        """A request for the client to create a new account.
 
         Subclasses implementing this function should raise a NotImplementedError if an address
         was provided that the client is unable to create an account at that address
@@ -121,7 +136,7 @@ class EthenoClient(object):
         :param balance: The initial balance for the account
         :param address: The address for the account, or None if the address should be auto-generated
         :return: returns the address of the account created
-        '''
+        """
         raise NotImplementedError('Clients must extend this function')
 
     def shutdown(self):
@@ -141,45 +156,52 @@ class EthenoClient(object):
     def short_name(self, name):
         self._short_name = name
 
+
 class SelfPostingClient(EthenoClient):
     def __init__(self, client):
         self.client = client
-        self._accounts = None
+        self._accounts: Optional[List[int]] = None
         self._created_account_index = -1
         # maintain a set of failed transactions so we know not to block on eth_getTransactionReceipt
-        self._failed_transactions = set()
-    def create_account(self, balance = 0, address = None):
+        self._failed_transactions: Set[str] = set()
+
+    def create_account(self, balance: int = 0, address: Optional[int] = None):
         if address is not None:
             raise NotImplementedError()
         if self._accounts is None:
-            self._accounts = list(map(lambda a : int(a, 16), self.post({
+            self._accounts = list(int(a, 16) for a in self.post({
                 'id': 1,
                 'jsonrpc': '2.0',
                 'method': 'eth_accounts'
-            })['result']))
+            })['result'])
         self._created_account_index += 1
         return self._accounts[self._created_account_index]
 
     def wait_until_running(self):
         pass
 
-    def post(self, data):
+    def post(self, data: Dict[str, Any]) -> Optional[Dict[str, Any]]:
         ret = self.client.post(data)
         if ret is not None and 'error' in ret:
-            if 'method' in data and (data['method'] == 'eth_sendTransaction' or data['method'] == 'eth_sendRawTransaction'):
-                if self.etheno.master_client != self and self.etheno.rpc_client_result and not isinstance(self.etheno.rpc_client_result, JSONRPCError) and 'result' in self.etheno.rpc_client_result:
-                    self.logger.error("%s: Failed transaction associated with master client transaction %s" % (self, self.etheno.rpc_client_result['result']))
+            if 'method' in data and (
+                    data['method'] == 'eth_sendTransaction' or data['method'] == 'eth_sendRawTransaction'
+            ):
+                if self.etheno.master_client != self and self.etheno.rpc_client_result \
+                        and not isinstance(self.etheno.rpc_client_result, JSONRPCError) \
+                        and 'result' in self.etheno.rpc_client_result:
+                    self.logger.error(f"{self!s}: Failed transaction associated with master client transaction "
+                                      f"{self.etheno.rpc_client_result['result']}")
                     self._failed_transactions.add(self.etheno.rpc_client_result['result'].lower())
             # TODO: Figure out a better way to handle JSON RPC errors
             raise JSONRPCError(self, data, ret)
         return ret
 
-    def estimate_gas(self, transaction):
-        '''
-        Estimates the gas cost for the given transaction or call
+    def estimate_gas(self, transaction: Dict[str, Any]) -> int:
+        """Estimates the gas cost for the given transaction or call
+
         :param transaction: a dict containing the entire transaction as if it were to be sent to `post()`
         :return: the gas cost in wei as an int
-        '''
+        """
         return int(self.post({
             'id': 1,
             'jsonrpc': '2.0',
@@ -187,21 +209,21 @@ class SelfPostingClient(EthenoClient):
             'params': transaction['params']
         })['result'], 16)
 
-    def get_gas_price(self):
+    def get_gas_price(self) -> int:
         return int(self.post({
             'id': 1,
             'jsonrpc': '2.0',
             'method': 'eth_gasPrice'
         })['result'], 16)
 
-    def get_net_version(self):
+    def get_net_version(self) -> int:
         return int(self.post({
             'id': 1,
             'jsonrpc': '2.0',
             'method': 'net_version'
         })['result'], 16)
 
-    def get_transaction_count(self, from_address):
+    def get_transaction_count(self, from_address) -> int:
         return int(self.post({
             'id': 1,
             'jsonrpc': '2.0',
@@ -210,11 +232,10 @@ class SelfPostingClient(EthenoClient):
         })['result'], 16)
 
     def wait_for_transaction(self, tx_hash):
-        '''
-        Blocks until the given transaction has been mined
+        """Blocks until the given transaction has been mined
         :param tx_hash: the transaction hash for the transaction to monitor
         :return: The transaction receipt
-        '''
+        """
         if isinstance(tx_hash, int):
             tx_hash = "0x%x" % tx_hash
         tx_hash = tx_hash.lower()
@@ -231,8 +252,11 @@ class SelfPostingClient(EthenoClient):
             time.sleep(5.0)
 
     def __str__(self):
-        return "%s[%s]" % (self.__class__.__name__, str(self.client))
-    __repr__ = __str__
+        return f"{self.__class__.__name__}[{self.client!s}]"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}[{self.client!r}]"
+
 
 class RpcProxyClient(SelfPostingClient):
     def __init__(self, rpcurl):
@@ -247,7 +271,7 @@ class RpcProxyClient(SelfPostingClient):
                 time.sleep(1.0)
                 self.logger.info(f"Retrying JSON RPC call to {self.client.urlstring}")
 
-    def is_running(self):
+    def is_running(self) -> bool:
         return webserver_is_up(self.client.urlstring)
 
     def wait_until_running(self):
@@ -258,7 +282,8 @@ class RpcProxyClient(SelfPostingClient):
             if slept % 5 == 0:
                 self.logger.info("Waiting for the client to start...")
 
-def QUANTITY(to_convert):
+
+def QUANTITY(to_convert: Optional[str]) -> Optional[int]:
     if to_convert is None:
         return None
     elif to_convert[:2] == '0x':
@@ -266,5 +291,6 @@ def QUANTITY(to_convert):
     else:
         return int(to_convert)
 
-def DATA(to_convert):
+
+def DATA(to_convert: Optional[str]) -> Optional[bytes]:
     return decode_hex(to_convert)
